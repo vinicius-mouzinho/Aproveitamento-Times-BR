b3fa4ef3c30ac20b0a0046f98a3acfc52b08df7c
Vinicius <viniciuscmouzinho@gmail.com>
2024-10-12 16:43:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import os
 from scrapingFunctions import (
     obter_gols_tempo_normal,
     salvar_dataframes_partidas_timesbr_2024,
@@ -9,15 +10,19 @@ from scrapingFunctions import (
     nomes_times_br_2024
 )
 
-# Função para carregar DataFrames atualizados com cache
-@st.cache(ttl=3600)  # Cache por 1 hora
+@st.cache(ttl=3600)
 def carregar_dataframes():
     salvar_dataframes_partidas_timesbr_2024(urls_br_2024, nomes_times_br_2024)
     dfs = []
     for nome_time in nomes_times_br_2024:
-        caminho_arquivo = f'Temporada {nome_time} 2024.csv'  # Nome do arquivo salvo na pasta
-        df = pd.read_csv(caminho_arquivo)
-        dfs.append(df)
+        caminho_arquivo = fr'C:\Users\Anderson\Football Analytics Docs\Temporada Times Brasileiros 2024\Temporada {nome_time} 2024.csv'
+        
+        # Verificar se o arquivo existe
+        if os.path.exists(caminho_arquivo):
+            df = pd.read_csv(caminho_arquivo)
+            dfs.append(df)
+        else:
+            st.error(f"Arquivo {caminho_arquivo} não encontrado.")
     return dfs
 
 # Carregar os DataFrames atualizados
@@ -28,8 +33,7 @@ st.title("Aplicativo de análise da Temporada das Equipes - Futebol Brasileiro")
 
 st.subheader(
     """
-    Aqui, você pode ver todos os jogos da temporada de uma equipe, além de uma tabela de aproveitamento com informações de gols, 
-    gols sofridos, vitórias, derrotas, empates e aproveitamento filtrado por data, mando de campo ou campeonato.
+    Aqui, você pode ver todos os jogos da temporada de uma equipe, além de uma tabela de aproveitamento com informações de gols, gols sofridos, vitórias, derrotas, empates e aproveitamento filtrado por data, mando de campo ou campeonato.
     """
 )
 
